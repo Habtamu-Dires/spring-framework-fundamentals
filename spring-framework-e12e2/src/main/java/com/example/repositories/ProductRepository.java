e6cf635db6d439ca54ba964d5ba7bece01c2041e
Habtamu Dires <habtadugna@gmail.com>
2023-12-16 16:40:01 +0300

    spring-data-jdbc support paging & sorting

--- spring-framework-e12e2/src/main/java/com/example/repositories/ProductRepository.java
+++ spring-framework-e12e2/src/main/java/com/example/repositories/ProductRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import java.util.List;
 
 public interface ProductRepository extends CrudRepository<Product, Integer> {
-    
+
     // no magic method name, you have to provide native queries, off curse no jpql here
     @Query("SELECT * FROM product WHERE name=:name")
     List<Product> findProductByName(String name);
